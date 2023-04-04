d100e5f4091801eb8f385699f07fc167e2e60ba7
Mihai Ionut Aftinescu <mihaiaftinescu@gmail.com>
2023-04-04 09:36:01 +0200

    updated build to correct spark sintax

--- spark/build.gradle.kts
+++ spark/build.gradle.kts
@@ -10,9 +10,6 @@
 plugins {
     // Apply the scala Plugin to add support for Scala.
     scala
-
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
 }
 
 repositories {
@@ -34,28 +31,4 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
-}
-
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use JUnit4 test framework
-            useJUnit("4.13.2")
-
-            dependencies {
-                // Use Scalatest for testing our library
-                implementation("org.scalatest:scalatest_2.13:3.2.13")
-                implementation("org.scalatestplus:junit-4-13_2.13:3.2.2.0")
-
-                // Need scala-xml at test runtime
-                runtimeOnly("org.scala-lang.modules:scala-xml_2.13:1.2.0")
-            }
-        }
-    }
-}
-
-application {
-    // Define the main class for the application.
-    mainClass.set("com.spark.App")
 }
